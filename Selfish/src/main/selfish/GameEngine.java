0794d5a8b14cea8ff9e5dbcfa1fee02eec3d0790
Wai Yan Aung <wya.aung@gmail.com>
2023-04-22 16:40:06 +0100

    Implemented All Functionalities of GameEngine Class
    - All GameEngineTests passed

--- Selfish/src/main/selfish/GameEngine.java
+++ Selfish/src/main/selfish/GameEngine.java
@@ -11,7 +11,11 @@ import selfish.deck.Card;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 
@@ -57,11 +61,48 @@ public class GameEngine implements Serializable{
         this.hasStarted = false;
     }
 
-    public int addPlayer(String player){return 0;}
+    public int addPlayer(String player){
+        if (this.getFullPlayerCount() == 5){
+            throw new IllegalStateException();
+        }
 
-    public int endTurn(){return 0;}
+        if (this.hasStarted){
+            throw new IllegalStateException();
+        }
+        Astronaut newPlayer = new Astronaut(player, this);
+        this.activePlayers.add(newPlayer);
+        return this.activePlayers.size();
+    }
 
-    public boolean gameOver(){return false;}
+    public int endTurn(){
+        if (!this.hasStarted){
+            throw new IllegalStateException();
+        }
+
+        if (this.currentPlayer != null){
+            if (this.currentPlayer.oxygenRemaining() == 0){
+                this.killPlayer(currentPlayer);
+            }else {
+                this.activePlayers.add(currentPlayer);
+            }
+            
+            this.currentPlayer = null;
+        }
+
+        return 0;
+    }
+
+    public boolean gameOver(){
+        if (this.corpses.size() == this.getFullPlayerCount()){
+            return true;
+        }
+
+        if (this.getWinner() != null){
+            return true;
+        }
+
+        return false;
+    }
 
     public List<Astronaut> getAllPlayers(){
         List<Astronaut> allPlayers = new ArrayList<Astronaut>(this.activePlayers);
@@ -74,11 +115,11 @@ public class GameEngine implements Serializable{
     }
 
     public Astronaut getCurrentPlayer(){
-        return currentPlayer;
+        return this.currentPlayer;
     }
 
     public int getFullPlayerCount(){
-        return 0;
+        return this.getAllPlayers().size();
     }
 
     public GameDeck getGameDeck(){
@@ -98,27 +139,142 @@ public class GameEngine implements Serializable{
     }
 
     public Astronaut getWinner(){
-        return null;
+        return this.activePlayers.stream()
+                .filter(player -> player.hasWon()).findFirst().orElse(null);
     }
 
     public void killPlayer(Astronaut corpse){
+        this.corpses.add(corpse);
+        this.activePlayers.remove(corpse);
+        if (corpse.equals(this.getCurrentPlayer())){
+            this.currentPlayer = null;
+        }
     }
 
-    public static GameEngine loadState(String path){
-        return null;
+    public static GameEngine loadState(String path) throws GameException{
+        GameEngine data = null;
+
+        try (FileInputStream fileIn = new FileInputStream(path);
+            ObjectInputStream in = new ObjectInputStream(fileIn)) {
+
+            data = (GameEngine) in.readObject();
+
+        } catch (IOException | ClassNotFoundException e) {
+            throw new GameException("", e);
+        }
+
+        return data;
     }
 
-    public void mergeDecks(Deck deck1, Deck deck2){}
+    public void mergeDecks(Deck deck1, Deck deck2){
+        int secondDeckSize = deck2.size();
+        while (secondDeckSize > 0){
+            deck1.add(deck2.draw());
+            secondDeckSize--;
+        }
+        deck1.shuffle(this.random);
+    }
 
-    public void saveState(String path){}
+    public void saveState(String path) throws GameException{
+        try {
+            FileOutputStream fileOutput = new FileOutputStream(path);
+            ObjectOutputStream objectOutput = new ObjectOutputStream(fileOutput);
+            objectOutput.writeObject(this);
+            objectOutput.close();
+            fileOutput.close();
+        } catch (IOException e) {
+            throw new GameException("", e);
+        }
+    }
 
     public Oxygen[] splitOxygen(Oxygen dbl){
-        return null;
+        Oxygen deckOxygenOne = null;
+        Oxygen discardOxygenOne = null;
+
+        try {
+            deckOxygenOne = this.gameDeck.drawOxygen(1);
+        } catch (IllegalStateException e) {
+            Oxygen[] oxygenCards = this.gameDiscard.splitOxygen(dbl);
+            return oxygenCards;
+        }
+        
+        try {
+            discardOxygenOne = this.gameDiscard.drawOxygen(1);
+        } catch (IllegalStateException e) {
+            if (deckOxygenOne != null){
+                discardOxygenOne = this.gameDeck.drawOxygen(1);
+                this.gameDeck.add(dbl);
+
+                return new Oxygen[]{deckOxygenOne, discardOxygenOne};
+            }
+
+            Oxygen[] oxygenCards = this.gameDeck.splitOxygen(dbl);
+            return oxygenCards;
+        }
+
+        this.gameDeck.add(dbl);
+        return new Oxygen[]{deckOxygenOne, discardOxygenOne};
     }
 
-    public void startGame(){}
+    public void startGame(){
+        int allPlayersSize = this.getAllPlayers().size();
+        if (allPlayersSize > 5 || allPlayersSize == 1){
+            throw new IllegalStateException();
+        }
 
-    public void startTurn(){}
+        if (this.hasStarted){
+            throw new IllegalStateException();
+        }
 
-    public Card travel(Astronaut traveller){return null;}
+        for (Astronaut player: this.activePlayers){
+            Oxygen oxygenTwoCard = this.gameDeck.drawOxygen(2);
+            player.addToHand(oxygenTwoCard);
+            for (int i = 0; i < 4; i++){
+                Oxygen oxygenOneCard = this.gameDeck.drawOxygen(1);
+                player.addToHand(oxygenOneCard);
+            }
+        }
+
+        for (int i = 0; i < 4; i++){
+            for (Astronaut player: this.activePlayers){
+                Card card = this.gameDeck.draw();
+                player.addToHand(card);
+            }
+        }
+
+        this.hasStarted = true;
+    }
+
+    public void startTurn(){
+        if (!this.hasStarted){
+            throw new IllegalStateException("HAS NOT STARTED");
+        }
+
+        if (this.gameOver()){
+            throw new IllegalStateException("GAME OVER");
+        }
+
+        if (this.currentPlayer != null){
+            throw new IllegalStateException("ADSADASDS");
+        }
+
+        currentPlayer = activePlayers.iterator().next();
+        currentPlayer.addToHand(gameDeck.draw());
+        activePlayers.remove(currentPlayer);
+    }
+
+    public Card travel(Astronaut traveller){
+        traveller.siphon();
+        traveller.siphon();
+
+        Card card = spaceDeck.draw();
+        if (!card.toString().equals(SpaceDeck.GRAVITATIONAL_ANOMALY)){
+            traveller.addToTrack(card);
+            if (traveller.hasWon()){
+                activePlayers.remove(traveller);
+            }
+        }
+
+        return card;
+    }
 }
