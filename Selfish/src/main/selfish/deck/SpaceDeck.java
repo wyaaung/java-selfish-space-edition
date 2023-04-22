229d62cb1caf00fd92fc0f66a479cd75cabae99c
Wai Yan Aung <wya.aung@gmail.com>
2023-04-22 04:24:47 +0100

    Documented SpaceDeck Class
    Test Passed:
    - testAsteroidFieldDocumented()
    - testUsefulJunkDocumented()
    - testCosmicRadiationDocumented()
    - testAllMembersDocumented()
    - testHyperspaceDocumented()
    - testMeteoroidDocumented()
    - testBlankSpaceDocumented()
    - testGravitationalAnomalyDocumented()
    - testWormholeDocumented()
    - testMysteriousNebulaDocumented()
    - testConstructorDocumented()
    - testSolarFlareDocumented()

--- Selfish/src/main/selfish/deck/SpaceDeck.java
+++ Selfish/src/main/selfish/deck/SpaceDeck.java
@@ -4,16 +4,39 @@ import java.util.List;
 
 import selfish.GameException;
 
+/** SpaceDeck Class (Extended from Deck Class)
+ *  @author Wai Yan Aung
+ *  @version 1
+ */
 public class SpaceDeck extends Deck{
+    // ASTEROID_FIELD's name
     public static final String ASTEROID_FIELD = "Asteroid field";
+
+    // BLANK_SPACE's name
     public static final String BLANK_SPACE = "Blank space";
+
+    // COSMIC_RADIATION's name
     public static final String COSMIC_RADIATION = "Cosmic radiation";
+
+    // GRAVITATIONAL_ANOMALY's name
     public static final String GRAVITATIONAL_ANOMALY = "Gravitational anomaly";
+
+    // HYPERSPACE's name
     public static final String HYPERSPACE = "Hyperspace";
+
+    // METEOROID's name
     public static final String METEOROID = "Meteoroid";
+
+    // MYSTERIOUS_NEBULA's name
     public static final String MYSTERIOUS_NEBULA = "Mysterious nebula";
+
+    // SOLAR_FLARE's name
     public static final String SOLAR_FLARE = "Solar flare";
+
+    // USEFUL_JUNK's name
     public static final String USEFUL_JUNK = "Useful junk";
+
+    // WORMHOLE's name
     public static final String WORMHOLE = "Wormhole";
 
     private static final long serialVersionUID = 5016812401135889608L;
