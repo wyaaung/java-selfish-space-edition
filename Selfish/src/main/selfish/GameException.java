47439ea30ee3d1da5b9549d3112d736ba2c6f860
Wai Yan Aung <wya.aung@gmail.com>
2023-04-22 17:18:33 +0100

    GameException Class Fully Documented
    - All JavadocGameExceptionTests passed

--- Selfish/src/main/selfish/GameException.java
+++ Selfish/src/main/selfish/GameException.java
@@ -1,7 +1,16 @@
 package selfish;
 
+/** GameException Class
+ *  @author Wai Yan Aung
+ *  @version 1
+ */
 public class GameException extends Exception{
 
+    /**
+     * GameException's constructor
+     * @param sIn The detail message.
+     * @param e Throwable exception
+     */
     public GameException(String sIn, Throwable e){
         super(sIn, e);
     }
